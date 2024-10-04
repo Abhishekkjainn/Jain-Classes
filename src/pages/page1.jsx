6c0efa660a5b53b8dbf131453cab32a207b6dce4
abhishekkjainn <jainabhishek1904@gmail.com>
2024-10-04 16:07:01 +0530

    Added the banner

---
+++ src/pages/page1.jsx
@@ -0,0 +1,25 @@
+export default function Page1() {
+  return (
+    <div className="page1">
+      <div className="banner1">
+        <div className="firstbanner">
+          <div className="bannertagdiv">
+            <div className="bannertag">
+              <div className="tag"></div>
+              <div className="name">Shailee's Jain Classes</div>
+            </div>
+          </div>
+          <div className="banner1heading">
+            Empowering Young Minds for Future Success.
+          </div>
+          <div className="banner1subhead">
+            From personalized teaching plans to consistent support, we are fully
+            committed to helping students overcome obstacles, achieve high
+            marks, and build a successful academic future
+          </div>
+        </div>
+        <div className="secondbanner"></div>
+      </div>
+    </div>
+  );
+}
